a99d2f23397c7abf880a8ac2dd3699d3ca9febcc
Christopher <leechrs1@gmail.com>
2022-07-14 23:54:11 -0500

    fixed all 404 errors

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,18 +3,18 @@ const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
-    './public/index.html',
-    './public/css/style.css',
+    'index.html',
+    './css/styles.css',
     // Don't need images due to cache limit?
-    './public/icons/icon-72x72.png',
-    './public/icons/icon-96x96.png',
-    './public/icons/icon-128x128.png',
-    './public/icons/icon-144x144.png',
-    './public/icons/icon-155x152.png',
-    './public/icons/icon-192x192.png',
-    './public/icons/icon-384x384.png',
-    './public/icons/icon-512x512.png',
-    './public/js.index.js'
+    '/icons/icon-72x72.png',
+    '/icons/icon-96x96.png',
+    '/icons/icon-128x128.png',
+    '/icons/icon-144x144.png',
+    '/icons/icon-152x152.png',
+    '/icons/icon-192x192.png',
+    '/icons/icon-384x384.png',
+    '/icons/icon-512x512.png',
+    './js/index.js'
 ];
 
 self.addEventListener('install', function (e) {
