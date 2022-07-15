7e158bddb98f42695218e3836fe875e3a52dae00
Christopher <leechrs1@gmail.com>
2022-07-14 23:13:35 -0500

    trying toi fix uncaught promise error

--- public/service-worker.js
+++ public/service-worker.js
@@ -19,7 +19,7 @@ const FILES_TO_CACHE = [
 
 self.addEventListener('install', function (e) {
     e.waitUntil(
-        caches.open(CACHE_NAME).then(function (cahce) {
+        caches.open(CACHE_NAME).then(function (cache) {
             console.log('installing cache : ' + CACHE_NAME);
             return cache.addAll(FILES_TO_CACHE)
         })
